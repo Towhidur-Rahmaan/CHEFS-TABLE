59207c47bda415e8cfab497fa4db2fd013e8d9a4
Towhidur-Rahmaan <shaontalukdar0@gmail.com>
2026-01-03 21:02:42 +0600

    Sidebar added

---
+++ src/components/pages/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Sidebar = () => {
+    return (
+        <div className='card bg-base-100 shadow-xl p-6'>
+            <h3>Want to cook:</h3>
+            
+        </div>
+    );
+};
+
+export default Sidebar;
